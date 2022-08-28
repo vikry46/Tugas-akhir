df8937e8c7f6a55154deb2faac862352303ffc51
vikryZoya <vikryverdinal327@gmail.com>
2022-08-28 17:03:21 +0700

    menghapus data

--- routes/web.php
+++ routes/web.php
@@ -42,9 +42,6 @@ Route::group(['prefix' => '/dashboard'], function(){
 
 Route::resource('/sosial', SosialController::class);
 
-//route full kalendar
-// perubahan baru
-// perunahan vikry
 
 });
 
